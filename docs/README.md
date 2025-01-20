f4ea987730b013255a14b9821519a03d4963d8e0
Muhd Juraimi <muhd.juraimi123@gmail.com>
2025-01-20 21:59:16 +0800

    refactor: rename chatbot
    Renamed chatbot from Duke to Donezo. Replaced all traces of Duke with
    Donezo

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Donezo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Donezo.java
@@ -0,0 +1,16 @@
+public class Donezo {
+    public static void main(String[] args) {
+        String logo =
+        " ______   _______  _        _______  _______  _______ \n" +
+        "(  __  \\ (  ___  )( (    /|(  ____ \\/ ___   )(  ___  )\n" +
+        "| (  \\  )| (   ) ||  \\  ( || (    \\/\\/   )  || (   ) |\n" +
+        "| |   ) || |   | ||   \\ | || (__        /   )| |   | |\n" +
+        "| |   | || |   | || (\\ \\) ||  __)      /   / | |   | |\n" +
+        "| |   ) || |   | || | \\   || (        /   /  | |   | |\n" +
+        "| (__/  )| (___) || )  \\  || (____/\\ /   (_/\\| (___) |\n" +
+        "(______/ (_______)|/    )_)(_______/(_______/(_______)\n";
+        
+        System.out.println("Hello from \n" + logo + "\n" + "What Can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
