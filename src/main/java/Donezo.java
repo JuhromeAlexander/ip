dc3792077bb15d64f09e10a51e0cf1e8104ca9f4
Muhd Juraimi <muhd.juraimi123@gmail.com>
2025-01-20 23:53:55 +0800

    feat: Add ability to mark task as complete and incomplete

--- src/main/java/Donezo.java
+++ src/main/java/Donezo.java
@@ -13,18 +13,20 @@ public class Donezo {
         "| (__/  )| (___) || )  \\  || (____/\\ /   (_/\\| (___) |\n" +
         "(______/ (_______)|/    )_)(_______/(_______/(_______)\n";
 
-    ArrayList<String> tasksAL;
+    ArrayList<Task> tasksAL;
     public static void main(String[] args) {
         Donezo donezo = new Donezo();
         donezo.run();
     }
     
-    public void run() {
+    private void run() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello from \n" + this.logo + "\n" + "What Can I do for you?");
+
+        int numTasks = 0;
         
         String inputString = scanner.nextLine();
-        tasksAL = new ArrayList<String>();
+        tasksAL = new ArrayList<Task>();
 
         while(!inputString.equals("bye")) {
             if (inputString.equals("list")) {
@@ -35,12 +37,35 @@ public class Donezo {
                 inputString = scanner.nextLine();
                 continue;
             }
-            tasksAL.add(inputString);
-            System.out.println("added: " + inputString);
+
+            if (inputString.contains("mark") || inputString.contains("unmark")) {
+                updateTaskStatus(inputString, tasksAL);
+                inputString = scanner.nextLine();
+                continue;
+            }
+
+            tasksAL.add(new Task(inputString));
+            System.out.println("added: " + tasksAL.get(numTasks).getDescription());
+            numTasks++;
             inputString = scanner.nextLine();
         }
 
         System.out.println("Peace out. Hope to see you again soon!\n");
         scanner.close();
+    }
+
+    private void updateTaskStatus(String userInput, ArrayList<Task> taskList) {
+        String[] userInputArr = userInput.split(" ");
+        int taskNdx = Integer.parseInt(userInputArr[1]) - 1;
+        Task affectedTask = taskList.get(taskNdx);
+
+        if (userInputArr[0].equals("mark")) {    
+            affectedTask.setDone(true);
+            System.out.println("Good. This task is now complete");
+        } else {   
+            affectedTask.setDone(false);
+            System.out.println("Really? You need to finish this soon. Marked as Undone");
+        }
+        System.out.println(affectedTask.toString());
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+//Code Adapted from Partial Solution Provided on the A-Classes Extension
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDone(boolean doneStatus) {
+        isDone = doneStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.getDescription();
+    }
+}
