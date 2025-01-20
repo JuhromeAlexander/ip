e548681da5dee0d9b22bde18f18a6e5500169917
Muhd Juraimi <muhd.juraimi123@gmail.com>
2025-01-20 22:21:57 +0800

    feat: add echo and exit functionality to chatbot

--- src/main/java/Donezo.java
+++ src/main/java/Donezo.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Donezo {
-    public static void main(String[] args) {
-        String logo =
+
+    String logo =
         " ______   _______  _        _______  _______  _______ \n" +
         "(  __  \\ (  ___  )( (    /|(  ____ \\/ ___   )(  ___  )\n" +
         "| (  \\  )| (   ) ||  \\  ( || (    \\/\\/   )  || (   ) |\n" +
@@ -9,8 +11,23 @@ public class Donezo {
         "| |   ) || |   | || | \\   || (        /   /  | |   | |\n" +
         "| (__/  )| (___) || )  \\  || (____/\\ /   (_/\\| (___) |\n" +
         "(______/ (_______)|/    )_)(_______/(_______/(_______)\n";
+    public static void main(String[] args) {
+        Donezo donezo = new Donezo();
+        donezo.run();
+    }
+    
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello from \n" + this.logo + "\n" + "What Can I do for you?");
         
-        System.out.println("Hello from \n" + logo + "\n" + "What Can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        String inputString = scanner.nextLine();
+
+        while(!inputString.equals("bye")) {
+            System.out.println(inputString);
+            inputString = scanner.nextLine();
+        }
+
+        System.out.println("Peace out. Hope to see you again soon!\n");
+        scanner.close();
     }
 }
